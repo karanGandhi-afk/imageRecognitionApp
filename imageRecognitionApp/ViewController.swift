634fb3a8317ff428ab4be9f8432e353506845dca
Jada White <jadaw.2021@gmail.com>
2021-04-13 12:57:21 -0400

    Added CoreML module and modified Main.storyboard.

--- imageRecognitionApp/ViewController.swift
+++ imageRecognitionApp/ViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
